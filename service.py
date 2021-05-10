a04ea6a865a2c59adf0a1f5417854885d09c3ae4
Gerasim Shaverdoff <gerasim@jewy.me>
2021-05-10 16:57:55 +0400

    edit service.py

--- service.py
+++ service.py
@@ -1,3 +1,5 @@
 from constants import ONLINE_STATE
-
-print(ONLINE_STATE)
+from constants import LOG_DIR
+print ("\n")
+print("The state of service is - "+ONLINE_STATE)
+print("log dir is - "+LOG_DIR)
